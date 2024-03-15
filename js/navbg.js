7863f5213c474dd2ed9ff39a8c1cc41a8a485734
ANGELDEVING <21340534@itnogales.edu.mx>
2024-03-15 14:22:51 -0700

    Se termino la navegacion

---
+++ js/navbg.js
@@ -0,0 +1,7 @@
+// JavaScript para mostrar/ocultar el menú en dispositivos móviles
+const menuToggle = document.querySelector('.menu-toggle');
+const menu = document.querySelector('.menu');
+
+menuToggle.addEventListener('click', () => {
+    menu.classList.toggle('show');
+});
